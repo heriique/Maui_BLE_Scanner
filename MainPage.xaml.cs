95b7b0a81a03aa220917018f7a0eab558e9f8018
ercr1608 <ercr1608@WIN-LKQN31MS8DB>
2023-02-20 15:42:21 +0100

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage(BluetoothScanner scanner)
+	{
+		InitializeComponent();
+
+		BindingContext= scanner;
+	}
+
+    private async Task<bool> CheckBluetoothStatus()
+    {
+        try
+        {
+            var requestStatus = await new BluetoothPermissions().CheckStatusAsync();
+            Debug.WriteLine("||||||CheckBluetoothStatus - return " + requestStatus.ToString());
+            return requestStatus == PermissionStatus.Granted;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine("||||||CheckBluetoothStatus - return FALLLSE");
+            // logger.LogError(ex);
+            return false;
+        }
+    }
+
+    public async Task<bool> RequestBluetoothAccess()
+    {
+        try
+        {
+
+            var requestStatus = await new BluetoothPermissions().RequestAsync();
+
+            Debug.WriteLine("||||||RequestBluetoothAccess - return " + requestStatus.ToString());
+            return requestStatus == PermissionStatus.Granted;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine("||||||RequestBluetoothAccess - return FALSSSE");
+            // logger.LogError(ex);
+            return false;
+        }
+    }
+
+    public async Task CheckAndRequestBluetoothPermission()
+    {
+        Debug.WriteLine("||||||||CheckAndREquestBLEPermission");
+        if (!await CheckBluetoothStatus())
+        {
+            await RequestBluetoothAccess();
+        }
+        await RequestBluetoothAccess();
+    }
+
+    protected async override void OnAppearing()
+    {
+        Console.WriteLine("Trying to enable bluetooth.");
+        Debug.WriteLine("||||||||||||||||||||||||||||||||||||||||||||||||");
+        await CheckAndRequestBluetoothPermission();
+    }
+
+}
+
---
+++ Platforms/Android/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+[assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
+
+namespace MauiApp1;
+
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
+{
+    protected override void OnPostResume()
+    {
+        base.OnPostResume();
+
+        // get permission for android >=12
+        //RequestPermissions(Permission)
+
+        //fast way to ensure we have location privileges
+        Geolocation.GetLastKnownLocationAsync();
+    }
+}
